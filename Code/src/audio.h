05a92e89c8665ebd0c75e4f3c8b26c2d06f1b654
Othniel <kylekojey@gmail.com>
2016-09-14 09:29:13 +0200

    AudioInit method is tested.

--- Code/src/audio.h
+++ Code/src/audio.h
@@ -15,19 +15,29 @@
 #define AUDIO_TASK_PRIORITY 1
 
 /*
+ * @brief Disable the DMA
+ */
+void AudioDisable(){
+	DMA_Cmd(DMA1_Stream5, DISABLE);
+	DAC_DMACmd(DAC_Channel_1,DISABLE);
+	DAC_Cmd(DAC_Channel_1, DISABLE);
+	TIM_Cmd(TIM6, DISABLE);
+}
+
+/*
  * @brief	Reset the DMA
  * @param	*DACBuffer		:	Array to be pushed to the DMA
  * 			DACBufferSize	:	Size of the array
  * 			DMA_Mode		:	Circular or Linear
  * @return 	None
  */
-static void vAudioDMATask(uint16_t *DACBuffer, uint32_t DACBufferSize, uint32_t DMA_Mode){
+void AudioDMA(uint16_t * DACBuffer, uint32_t DACBufferSize, uint32_t DMA_Mode){
 	// DMA configuration	[Channel_7, Stream_5]
 	DMA_InitTypeDef DMA_InitStruct;
-	DMA_StructInit(&DMA_InitStruct);
+	DMA_Cmd(DMA1_Stream5, DISABLE);
 	DMA_InitStruct.DMA_Channel=DMA_Channel_7;
 	DMA_InitStruct.DMA_PeripheralBaseAddr=(uint32_t)(DAC_BASE + 0x08);  //DAC channel1 12-bit right-aligned data holding register (ref manual pg. 264)
-	DMA_InitStruct.DMA_Memory0BaseAddr=(uint32_t)&DACBuffer;
+	DMA_InitStruct.DMA_Memory0BaseAddr=(uint32_t)DACBuffer;
 	DMA_InitStruct.DMA_DIR=DMA_DIR_MemoryToPeripheral;
 	DMA_InitStruct.DMA_BufferSize = DACBufferSize;
 	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
@@ -40,7 +50,7 @@ static void vAudioDMATask(uint16_t *DACBuffer, uint32_t DACBufferSize, uint32_t 
 	DMA_InitStruct.DMA_FIFOThreshold = DMA_FIFOThreshold_Full;
 	DMA_InitStruct.DMA_MemoryBurst = DMA_MemoryBurst_Single;
 	DMA_InitStruct.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
-	DMA_Init(DMA1_Stream5, &DMA_InitStructure);
+	DMA_Init(DMA1_Stream5, &DMA_InitStruct);
 	DMA_Cmd(DMA1_Stream5, ENABLE);
 }
 /*
@@ -52,23 +62,24 @@ static void vAudioDMATask(uint16_t *DACBuffer, uint32_t DACBufferSize, uint32_t 
  * 			preScaler		:	PSC
  * @return 	None
  */
-static void vAudioTask(uint16_t *DACBuffer, uint32_t DACBufferSize, uint32_t DMA_Mode, uint16_t wavPeriod, uint16_t preScaler){
+void AudioInit(uint16_t *DACBuffer, uint32_t DACBufferSize, uint32_t DMA_Mode, uint16_t wavPeriod, uint16_t preScaler){
+
 	// RCC configuration
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1 | RCC_AHB1Periph_GPIOA, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC | RCC_APB1Perith_TIM6, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC | RCC_APB1Periph_TIM6, ENABLE);
 
 	// GPIO configuration [GPIOA_4]
 	GPIO_InitTypeDef GPIO_InitStruct;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AN;
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIOPuPd_NOPULL;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4;
 	GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 	// TIMER configuration [TIM6]
 	TIM_TimeBaseInitTypeDef	TIM_TimeBaseStruct;
-	TIM_TimeBaseStruct.TIM_Period = wavPariod-1;
+	TIM_TimeBaseStruct.TIM_Period = wavPeriod-1;
 	TIM_TimeBaseStruct.TIM_Prescaler = preScaler-1;
 	TIM_TimeBaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
@@ -82,7 +93,7 @@ static void vAudioTask(uint16_t *DACBuffer, uint32_t DACBufferSize, uint32_t DMA
 	DMA_StructInit(&DMA_InitStruct);
 	DMA_InitStruct.DMA_Channel=DMA_Channel_7;
 	DMA_InitStruct.DMA_PeripheralBaseAddr=(uint32_t)(DAC_BASE + 0x08);  //DAC channel1 12-bit right-aligned data holding register (ref manual pg. 264)
-	DMA_InitStruct.DMA_Memory0BaseAddr=(uint32_t)&DACBuffer;
+	DMA_InitStruct.DMA_Memory0BaseAddr=(uint32_t)DACBuffer;
 	DMA_InitStruct.DMA_DIR=DMA_DIR_MemoryToPeripheral;
 	DMA_InitStruct.DMA_BufferSize = DACBufferSize;
 	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
@@ -95,13 +106,13 @@ static void vAudioTask(uint16_t *DACBuffer, uint32_t DACBufferSize, uint32_t DMA
 	DMA_InitStruct.DMA_FIFOThreshold = DMA_FIFOThreshold_Full;
 	DMA_InitStruct.DMA_MemoryBurst = DMA_MemoryBurst_Single;
 	DMA_InitStruct.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
-	DMA_Init(DMA1_Stream5, &DMA_InitStructure);
+	DMA_Init(DMA1_Stream5, &DMA_InitStruct);
 	DMA_Cmd(DMA1_Stream5, ENABLE);
 
 	// DAC configuration	[Channel_1,TIM6]
 	DAC_InitTypeDef DAC_InitStruct;
 	DAC_InitStruct.DAC_Trigger = DAC_Trigger_T6_TRGO;
-	DAC_InitStruct.WaveGeneration = DAC_WaveGeneration_None;
+	DAC_InitStruct.DAC_WaveGeneration = DAC_WaveGeneration_None;
 	DAC_InitStruct.DAC_LFSRUnmask_TriangleAmplitude = DAC_LFSRUnmask_Bit0;
 	DAC_InitStruct.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
 	DAC_Init(DAC_Channel_1, &DAC_InitStruct);
