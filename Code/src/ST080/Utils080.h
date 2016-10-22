b464d585edd41351bab6c12a81dac64997c6f61c
Tinotenda Chemvura <ttchemvura@gmail.com>
2016-10-22 13:05:01 +0200

    Fixed an error when config GPIOs. Updated mappings

--- Code/src/ST080/Utils080.h
+++ Code/src/ST080/Utils080.h
@@ -189,11 +189,11 @@ void startUpConfigs(){
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
 	// pins A8-10,13-15
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	// pins C1-3,6,8,9,13-15
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_8 | GPIO_Pin_6 | GPIO_Pin_9 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	// pins C0-11
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 
 	// pins E0-3
